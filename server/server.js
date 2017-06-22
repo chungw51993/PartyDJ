ee6fdb9f93604125531f3bb306f6b57d9b115c98
Won Jin Chung <chungw51993@gmail.com>
2017-06-21 20:45:05 -0500

    Added basic server

--- server/server.js
+++ server/server.js
@@ -1,2 +1,23 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const port = 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
+})
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.listen(port, () => {
+  console.log('Server is listening on ', port);
+})
