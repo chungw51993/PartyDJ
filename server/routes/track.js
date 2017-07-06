bac61176b82ab8de65df305214168a13fa82aa69
Won Jin Chung <chungw51993@gmail.com>
2017-07-05 19:18:49 -0500

    Added api routes for track

--- server/routes/track.js
+++ server/routes/track.js
@@ -2,4 +2,36 @@ const express = require('express');
 const router = express.Router();
 const Track = require('../db/controller/track');
 
+router.post('/:pid', (req, res) => {
+  let pid = req.params.pid;
+  let artist = req.body.artist;
+  let album = req.body.album;
+  let track = req.body.track;
+
+  Track.addTrack(artist, album, track, pid)
+    .then((data) => {
+      res.status(200);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send(err);
+    });
+});
+
+router.delete('/:pid', (req, res) => {
+  let pid = req.params.pid;
+  let tid = req.body.tid;
+
+  Track.deleteTrack(tid, pid)
+    .then((data) => {
+      res.status(200);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send(err);
+    });
+});
+
 module.exports = router;
