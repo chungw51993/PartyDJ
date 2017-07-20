e60ca59e227909e5fbb2be70fd8418965b257118
Won Jin Chung <chungw51993@gmail.com>
2017-07-19 20:00:05 -0500

    Working on some style and deleting track

--- client/directives/track-item.js
+++ client/directives/track-item.js
@@ -1,21 +1,38 @@
 angular.module('partyDJ')
 
-.controller('TrackItemCtrl', function() {
-
-})
-
 .directive('trackItem', function() {
   return {
     scope: {
-      track: '<'
+      track: '<',
+      user: '<',
+      playlist: '<',
+      service: '<'
     },
     restrict: 'E',
-    controller: 'TrackItemCtrl',
-    controllerAs: 'ctrl',
-    bindToController: true,
+    link: function(scope) {
+      scope.isAdmin = false;
+
+      scope.checkIfAdmin = () => {
+        if (scope.playlist.user_id === scope.user.spotify_id) {
+          scope.isAdmin = true;
+        }
+      };
+
+      scope.deleteTrack = () => {
+
+      };
+
+      scope.checkIfAdmin();
+    },
     template: `
-      <div>
-        <div>{{ ctrl.track.name }}</div>
+      <div class="row trackItem">
+        <img class="trackImg col-md-2 col-sm-2 col-lg-2" ng-src="{{ track.Album.image }}">
+        <div class="col-md-8 col-sm-8 col-lg-8">
+          <div class="trackName">Title: {{ track.name }}</div>
+          <div class="trackArtist">Artist: {{ track.Album.Artist.name }}</div>
+          <div class="trackAlbum">Album: {{ track.Album.name }}</div>
+        </div>
+        <button ng-if="isAdmin" class="trackDelete col-md-2 col-sm-2 col-lg-2">Delete</button>
       </div>
     `
   };
