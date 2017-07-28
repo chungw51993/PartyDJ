bfe38922ba54875260ce1c222d43cad1c2bad3be
Won Jin Chung <chungw51993@gmail.com>
2017-07-28 15:37:06 -0500

    Adding sockets to specific playlist so they don't all update

--- server/socket.js
+++ server/socket.js
@@ -3,12 +3,19 @@ module.exports = (io) => {
   io.on('connection', function(socket) {
     allConnection.push(socket);
     console.log('THERE IS CURRENTLY ', allConnection.length, ' SOCKETS CONNECTED');
-    const room = socket.handshake.headers.referer.split('/')[3];
-    console.log(socket.handshake.headers.referer.split('/')[3]);
+
+    const playlist = socket.handshake.headers.referer.split('/')[3];
+
+    socket.join(playlist);
+    console.log('PLAYLIST JOINED PLAYLIST: ', playlist);
 
     socket.on('new:track', (data) => {
       console.log('EMIT NEW TRACK');
-      io.sockets.emit('add:track', data);
+      io.sockets.to(playlist).emit('add:track', data);
+    });
+
+    socket.on('disconnect', () => {
+      allConnection.shift();
     });
 
   });
