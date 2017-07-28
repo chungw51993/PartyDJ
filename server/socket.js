204493349d5938be56d66aea137ab6d040c043bb
Won Jin Chung <chungw51993@gmail.com>
2017-07-28 14:14:58 -0500

    Socket emitting correctly to add song when one user adds a song

--- server/socket.js
+++ server/socket.js
@@ -2,9 +2,11 @@ module.exports = (io) => {
   const allConnection = [];
   io.on('connection', function(socket) {
     allConnection.push(socket);
-    console.log(allConnection.length, '<<<<<<<<<<<<<<<<<<<<<<<<<<');
+
     socket.on('new:track', (data) => {
-      socket.emit('add:track', data);
+      console.log('EMIT NEW TRACK');
+      io.sockets.emit('add:track', data);
     });
+
   });
 };
