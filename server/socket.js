09c1a07ec286e1e8274f69787658c91877f64e4a
Won Jin Chung <chungw51993@gmail.com>
2017-07-28 15:11:20 -0500

    Working on assigning socket rooms

--- server/socket.js
+++ server/socket.js
@@ -2,6 +2,9 @@ module.exports = (io) => {
   const allConnection = [];
   io.on('connection', function(socket) {
     allConnection.push(socket);
+    console.log('THERE IS CURRENTLY ', allConnection.length, ' SOCKETS CONNECTED');
+    const room = socket.handshake.headers.referer.split('/')[3];
+    console.log(socket.handshake.headers.referer.split('/')[3]);
 
     socket.on('new:track', (data) => {
       console.log('EMIT NEW TRACK');
