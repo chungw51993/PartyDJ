be7a9b86e56684bdb1fc60311bbc7571986ee7d3
Won Jin Chung <chungw51993@gmail.com>
2017-07-29 13:19:31 -0500

    Added disable to only let admin start the playlist

--- client/directives/music-player.js
+++ client/directives/music-player.js
@@ -54,7 +54,7 @@ angular.module('partyDJ')
       };
 
       scope.gongTrack = () => {
-        // scope.gonged = true;
+        scope.gonged = true;
         if (scope.gong < 2) {
           scope.gong++;
         } else {
@@ -74,8 +74,8 @@ angular.module('partyDJ')
             <span class="label">{{ current.Album.name }}</span>
           </div>
         </marquee>
-        <button class="play col-md-2 col-lg-2 col-sm-2" ng-click="playTrack(current)" ng-if="!playing">Play</button>
-        <button class="pause col-md-2 col-lg-2 col-sm-2" ng-click="pauseTrack(current)" ng-if="playing">Pause</button>
+        <button class="play col-md-2 col-lg-2 col-sm-2" ng-click="playTrack(current)" ng-if="!playing" ng-disabled="!isAdmin">Play</button>
+        <button class="pause col-md-2 col-lg-2 col-sm-2" ng-click="pauseTrack(current)" ng-if="playing" ng-disabled="!isAdmin">Pause</button>
         <div class="col-md-8 col-lg-8 col-sm-8"></div>
         <button class="next col-md-2 col-lg-2 col-sm-2" ng-click="nextTrack()" ng-if="isAdmin">Next</button>
         <button class="gong col-md-2 col-lg-2 col-sm-2" ng-click="gongTrack()" ng-if="!isAdmin" ng-disabled="gonged">Gong</button>
