36841e6e862aaf96e9ef486b768bed5575ce9036
Won Jin Chung <chungw51993@gmail.com>
2017-08-04 14:35:42 -0500

    Working on styling main page

--- client/directives/playlist-item.js
+++ client/directives/playlist-item.js
@@ -23,12 +23,13 @@ angular.module('partyDJ')
       };
     },
     template: `
-      <div class="playlist-item">
-        <div ng-click="goToDetail()" class="title">{{ playlist.name }}</div>
-        <div class="date">{{ playlist.createdAt.slice(0, 10) }}</div>
-        <button class="editPL" ng-click="showEditPopup()">Edit</button>
-        <button class="deletePL" ng-click="showDeletePopup()">Delete</button>
-        <hr>
+      <div class="playlist-item row">
+        <div class="col-10 col-sm-10 col-md-10 col-lg-10">
+          <div ng-click="goToDetail()" class="title">{{ playlist.name }}</div>
+          <div class="date">{{ playlist.createdAt.slice(0, 10) }}</div>
+        </div>
+        <button class="editPL mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" ng-click="showEditPopup()">Edit</button>
+        <button class="deletePL mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" ng-click="showDeletePopup()">Delete</button>
       </div>
     `
   };
