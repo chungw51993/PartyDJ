c8b0ebd984aec5a222363bab6519523bc71352b9
Won Jin Chung <chungw51993@gmail.com>
2017-08-04 15:03:08 -0500

    Adding share button on playlist

--- client/directives/playlist-item.js
+++ client/directives/playlist-item.js
@@ -26,10 +26,11 @@ angular.module('partyDJ')
     },
     template: `
       <div class="playlist-item row">
-        <div class="col-9 col-sm-9 col-md-10 col-lg-10">
+        <div class="col-8 col-sm-8 col-md-9 col-lg-9">
           <div ng-click="goToDetail()" class="title">Name: {{ playlist.name }}</div>
           <div class="date">Created on: {{ date }}</div>
         </div>
+        <button class="col-1 col-sm-1 sharePL mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">Share</button>
         <button class="editPL mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" ng-click="showEditPopup()">Edit</button>
         <button class="deletePL mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" ng-click="showDeletePopup()">Delete</button>
       </div>
