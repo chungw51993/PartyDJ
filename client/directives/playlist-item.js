0eaa462b10c7655831361b8182e650977624f792
Won Jin Chung <chungw51993@gmail.com>
2017-08-15 21:14:30 -0500

    Fixed date not changing properly

--- client/directives/playlist-item.js
+++ client/directives/playlist-item.js
@@ -11,8 +11,10 @@ angular.module('partyDJ')
     },
     restrict: 'E',
     link: (scope) => {
-      const date = moment(scope.playlist.createdAt.slice(0, 10)).format('MMMM Do YYYY');
-      console.log(date, scope.playlist.name);
+      scope.getDate = () => {
+        const date = moment(scope.playlist.createdAt.slice(0, 10)).format('MMMM Do YYYY');
+        return date;
+      };
 
       scope.showEditPopup = () => {
         scope.showEdit(scope.playlist);
@@ -29,14 +31,12 @@ angular.module('partyDJ')
       scope.goToDetail = () => {
         scope.redirect(scope.playlist.id);
       };
-
-      scope.date = date;
     },
     template: `
       <div class="playlist-item row">
         <div class="col-7 col-sm-7 col-md-8 col-lg-8">
           <div ng-click="goToDetail()" class="title">{{ playlist.name }}</div>
-          <div class="date">Created on: {{ date }}</div>
+          <div class="date">Created on: {{ getDate() }}</div>
         </div>
         <button class="deletePL mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" ng-click="showDeletePopup()">Delete</button>
         <button class="editPL mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" ng-click="showEditPopup()">Edit</button>
