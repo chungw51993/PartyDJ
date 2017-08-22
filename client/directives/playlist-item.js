0db091fea002543fcab636f504f970e755c26aa4
Won Jin Chung <chungw51993@gmail.com>
2017-08-21 20:16:30 -0500

    Changed font family for playlist page

--- client/directives/playlist-item.js
+++ client/directives/playlist-item.js
@@ -35,8 +35,8 @@ angular.module('partyDJ')
     template: `
       <div class="playlist-item row">
         <div class="col-7 col-sm-7 col-md-8 col-lg-8">
-          <div ng-click="goToDetail()" class="title">{{ playlist.name }}</div>
-          <div class="date">Created on: {{ getDate() }}</div>
+          <div ng-click="goToDetail()" class="playlist-title">{{ playlist.name }}</div>
+          <div class="playlist-date">Created on: {{ getDate() }}</div>
         </div>
         <button class="deletePL mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" ng-click="showDeletePopup()">Delete</button>
         <button class="editPL mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" ng-click="showEditPopup()">Edit</button>
