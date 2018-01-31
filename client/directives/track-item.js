57ada88962c03c2f4c3c5d788e14e6429f6f9fcc
Won Jin Chung <chungw51993@gmail.com>
2018-01-31 13:57:55 -0600

    Changed delete button

--- client/directives/track-item.js
+++ client/directives/track-item.js
@@ -19,13 +19,20 @@ angular.module('partyDJ')
     },
     template: `
       <div class="row trackItem">
-        <img class="trackImg col-md-2 col-sm-2 col-lg-2" ng-src="{{ track.Album.image }}">
+        <img
+          class="trackImg col-md-2 col-sm-2 col-lg-2"
+          ng-src="{{ track.Album.image }}"
+        >
         <div class="col-md-8 col-sm-8 col-lg-8">
           <div class="trackName">Title: {{ track.name }}</div>
           <div class="trackArtist">Artist: {{ track.Album.Artist.name }}</div>
           <div class="trackAlbum">Album: {{ track.Album.name }}</div>
         </div>
-        <button ng-if="isAdmin" ng-click="deleteTrack()" class="trackDelete col-md-2 col-sm-2 col-lg-2">Delete</button>
+        <button
+          ng-if="isAdmin"
+          ng-click="deleteTrack()"
+          class="trackDelete mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent col-md-2 col-sm-2 col-lg-2"
+          >Delete</button>
       </div>
     `
   };
